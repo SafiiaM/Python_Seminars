20fb906c0b1cb72ae85f845dbc5d0ac6e6446aa1
SafiiaM <107404606+SafiiaM@users.noreply.github.com>
2022-09-19 18:18:01 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -7,26 +7,5 @@ lst = [i for i in txt.split() if find_txt not in i]
 print(f'Результат: {" ".join(lst)}')
 
 
-# with open("words.txt", "r") as fin:
-#     for line in fin:
-#         words = line.split()
-#         for word in words:
-#             if "абв" in word:
-#                 words.remove(word)
-#         sentence = " ".join(words)
-#         print(sentence)
 
-# with open('tex.txt','r') as n:
-# lst = [int(i) for i in n.readline().split()]  
-# for i in range(1,len(lst)):
-#         if lst[i] - lst[i - 1] > 1:
-#             print(lst[i]-1)
-
-
-# def del_some_words(my_text):
-#     my_text = list(filter(lambda x:'абв' not in x, my_text.split()))
-#     return " ".join(my_text)
-
-# my_text = del_some_words(my_text)
-# print(my_text)
 
